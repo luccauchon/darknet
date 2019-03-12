fd773a76b2775fc30b4eb5462cf7df15658da7cb
cj3272 <luccauchon@gmail.com>
2019-03-12 04:57:49 -0400

    Lache pas.

--- TRAIN.sh
+++ TRAIN.sh
@@ -1,2 +1,2 @@
 mkdir backup
-./darknet detector train data/odema.data cfg/yolo-voc.2.odema.cfg -gpus 1
+./darknet detector train data/odema.data cfg/yolo-voc.2.odema.cfg -gpus 0
--- VALIDATE.sh
+++ VALIDATE.sh
@@ -1,12 +1,12 @@
 : "${VAL_GPU?GPU to use.}": "${BACKUPDIR?directory for weights.}": "${OUTPUTFILE?result file.}" 
-
+echo 'Using GPU ' $VAL_GPU ' with models located at ' $BACKUPDIR ' and results in file ' $OUTPUTFILE
 for value in {100..1000..100}
 do    
   echo $value    
   ./darknet -i $VAL_GPU detector recall data/odema.data cfg/yolo-voc.2.odema.cfg $BACKUPDIR/yolo-voc_$value.weights &>> $OUTPUTFILE
 done
 
-for value in {1000..60000..1000}
+for value in {1000..90000..1000}
 do
   echo $value
   ./darknet -i $VAL_GPU detector recall data/odema.data cfg/yolo-voc.2.odema.cfg $BACKUPDIR/yolo-voc_$value.weights &>> $OUTPUTFILE
